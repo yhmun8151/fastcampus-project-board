3090e8768cc8088b07a6f238962fd7de8cb0b186
jinyoung <yhmun8151@gmail.com>
2022-10-18 23:06:43 +0900

    #8 - 도메인 클래스 설계 (게시글,댓글)

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -10,5 +10,5 @@ public class ArticleComment {
     private LocalDateTime createdAt; // 생성일시
     private String createdBy; // 생성자
     private LocalDateTime modifiedAt; // 수정일시
-    private String modifiedBy; // 수정자 .
+    private String modifiedBy; // 수정자
 }
